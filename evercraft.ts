e6b5db25d54feff314facf6344b9fada27dd8ae4
Fr3gU <fr3gu77@gmail.com>
2020-09-28 14:46:04 +0200

    Alignment, ArmorClass and HitPoints done

--- evercraft.ts
+++ evercraft.ts
@@ -1,7 +1,32 @@
 export class Hero {
+    private _alignment: Alignment;
+    name: string;
+    armorClass: any;
+    hitPoints: any;
     constructor() {
         this.name = "";
+        this._alignment = Alignment.Neutral;
+        this.armorClass = 10;
+        this.hitPoints = 5;
     }
+    
+    public get alignment() : Alignment {
+        return this._alignment;
+    }
+    
+    public set alignment(v : Alignment) {
+        const vals = Object.values(Alignment);
+        const found = !!vals.find(u => u === v);
+        if(!found) {
+            throw `Invalid value (${v})!`;
+        }
+        
+        this._alignment = v;
+    }
+}
 
-    name: string;
+export enum Alignment {
+    Neutral = 1,
+    Good = 2,
+    Evil = 3
 }
--- specs/hero-spec.ts
+++ specs/hero-spec.ts
@@ -1,4 +1,8 @@
-import { Hero } from "../evercraft";
+/**
+ * @jest-environment node
+ */
+
+import { Alignment, Hero } from "../evercraft";
 
 describe("Hero", () => {
 
@@ -14,6 +18,37 @@ describe("Hero", () => {
         it("can be changed", () => {
             sub.name = "Alvar";
             expect(sub.name).toBe("Alvar");
+        });
+    });
+
+    describe("#alignment", () => {
+        it("defaults to Neutral", () => {
+            expect(sub.alignment).toBe(Alignment.Neutral);
+        });
+
+        it.each([
+            [Alignment.Neutral, "Neutral"],
+            [Alignment.Good, "Good"],
+            [Alignment.Evil, "Evil"]
+        ])("can be set to %s (%s)", (alignment, _expected) => {
+            sub.alignment = alignment;
+            expect(sub.alignment).toBe(alignment);
+        });
+
+        it("throws on setting invalid value", () => {
+            expect(() => sub.alignment = 0).toThrowError("Invalid value (0)!");
+        });
+    });
+
+    describe("#armorClass", () => {
+        it("defaults to 10", () => {
+            expect(sub.armorClass).toBe(10);
+        });
+    });
+
+    describe("#hitPoints", () => {
+        it("defaults to 5", () => {
+            expect(sub.hitPoints).toBe(5);
         });
     });
 });
